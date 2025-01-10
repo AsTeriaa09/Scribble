1d23c0b6032f02fcbbc2c8e9fceb6a1f167953b7
AsTeriaa09 <mahiraakhter950@gmail.com>
2025-01-10 16:18:15 +0600

    add: authorization using clerk

---
+++ src/components/header.jsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import { PenBox, FolderOpen } from "lucide-react";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import UserMenu from "./user-menu";
+const Header = () => {
+  return (
+    <header>
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link href={"/"} className="text-3xl  text-orange-700">
+          <h1>Scribble</h1>
+        </Link>
+        <div className="flex items-center gap-4">
+          <SignedIn>
+            <Link href="/dashboard#collections">
+              <Button variant="outline" className="flex items-center gap-2">
+                <FolderOpen size={18} />
+                <span className="hidden md:inline">Collections</span>
+              </Button>
+            </Link>
+          </SignedIn>
+
+          <Link href="/journal/write">
+            <Button variant="journal" className="flex items-center gap-2">
+              <PenBox size={18} />
+              <span className="hidden md:inline">Write New</span>
+            </Button>
+          </Link>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
+              <Button variant="outline">
+                Login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserMenu />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
