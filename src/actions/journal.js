0053cc17d539e4f2c64f0c4270306889e813f0f1
AsTeriaa09 <mahiraakhter950@gmail.com>
2025-02-01 20:24:15 +0600

    add: get draft logic using actions

--- src/actions/journal.js
+++ src/actions/journal.js
@@ -256,3 +256,26 @@ export async function updateJournalEntry(data) {
     throw new Error(error.message);
   }
 }
+
+export async function getDraft() {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const draft = await db.draft.findUnique({
+      where: { userId: user.id },
+    });
+
+    return { success: true, data: draft };
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+}
